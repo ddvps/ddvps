0b10d251550d9cf1ca43dda5809f434c17558315
ddvps <1105880954@qq.com>
2024-12-26 18:35:41 +0800

    Update NewReinstall.sh

--- NewReinstall.sh
+++ NewReinstall.sh
@@ -156,7 +156,7 @@ function Start() {
   if [[ "$isCN" == '1' ]]; then
    wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://cdn.jsdelivr.net/gh/ddvps/ddvps@master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
   else 
-   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://s.id/ddvps' && chmod a+x /tmp/InstallNET.sh
+   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://raw.githubusercontent.com/ddvps/ddvps/master/NewReinstall.sh' && chmod a+x /tmp/InstallNET.sh
   fi
   
   CMIRROR=''
